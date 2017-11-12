f2eaf00bc4466069de80d6b9c38b4510aa789c4c
MoerkerkeLander <lander.moerkerke@howest.student.be>
2017-11-12 14:40:55 +0100

    Moved MainPage to View folder

---
+++ ASOIAF/ASOIAF/View/OverviewPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ASOIAF.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ASOIAF
+{
+	public partial class OverviewPage : ContentPage
+	{
+		public OverviewPage()
+		{
+			InitializeComponent();
+
+			TestGet();
+		}
+
+		private async void TestGet()
+		{
+			try
+			{
+				List<House> collection = await WesterosManager.GetHousesAsync();
+
+				foreach (House item in collection)
+				{
+					Debug.WriteLine(item.Name);
+				}
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(e.Message);
+				throw;
+			}
+			
+		}
+	}
+}
