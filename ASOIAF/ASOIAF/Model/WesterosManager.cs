0a66b882950e057685e955054e658236a48e54ed
MoerkerkeLander <lander.moerkerke@howest.student.be>
2017-11-12 14:40:25 +0100

    Added methods for importing json data

--- ASOIAF/ASOIAF/Model/WesterosManager.cs
+++ ASOIAF/ASOIAF/Model/WesterosManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ASOIAF.Model
 {
 	public static class WesterosManager
@@ -14,7 +13,7 @@ namespace ASOIAF.Model
 		{
 			HttpClient client = new HttpClient();
 			client.DefaultRequestHeaders.Add("Accept", "application/json");
-
+			
 			string result = await client.GetStringAsync("https://www.anapioficeandfire.com/api/books");
 
 			if (result == null)
@@ -24,6 +23,38 @@ namespace ASOIAF.Model
 
 			List<Book> books = JsonConvert.DeserializeObject<List<Book>>(result);
 			return books;
+		}
+
+		public static async Task<List<Character>> GetCharactersAsync()
+		{
+			HttpClient client = new HttpClient();
+			client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+			string result = await client.GetStringAsync("https://www.anapioficeandfire.com/api/characters");
+
+			if (result == null)
+			{
+				return null;
+			}
+
+			List<Character> characters = JsonConvert.DeserializeObject<List<Character>>(result);
+			return characters;
+		}
+
+		public static async Task<List<House>> GetHousesAsync()
+		{
+			HttpClient client = new HttpClient();
+			client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+			string result = await client.GetStringAsync("https://www.anapioficeandfire.com/api/houses");
+
+			if (result == null)
+			{
+				return null;
+			}
+
+			List<House> houses = JsonConvert.DeserializeObject<List<House>>(result);
+			return houses;
 		}
 	}
 }
