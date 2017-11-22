60889e250960738e3a206d7fa7c36b6d55f9505b
MoerkerkeLander <lander.moerkerke@howest.student.be>
2017-11-22 17:23:03 +0100

    Finished CharacterDetail

--- ASOIAF/ASOIAF/View/CharacterDetail.xaml.cs
+++ ASOIAF/ASOIAF/View/CharacterDetail.xaml.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,52 +13,161 @@ namespace ASOIAF.View
 	public partial class CharacterDetail : ContentPage
 	{
 		public static List<Character> ListCharacters { get; set; }
+		private static List<House> ListHouses;
 
 		public CharacterDetail(Character pSelectedCharacter)
 		{
 			InitializeComponent();
-
 			LoadCharacter(pSelectedCharacter);
+			btnBack.Pressed += BtnBack_Pressed;
+		}
+
+		private void BtnBack_Pressed(object sender, EventArgs e)
+		{
+			Navigation.PopAsync();
 		}
 
 		private void LoadCharacter(Character pSelecteredCharacter)
 		{
 			lblName.Text = pSelecteredCharacter.Name;
-			lblGender.Text = pSelecteredCharacter.Gender;
-			lblBorn.Text = pSelecteredCharacter.Born;
-			lblCulture.Text = pSelecteredCharacter.Culture;
 
-			foreach (string alias in pSelecteredCharacter.Aliases)
+			List<string> aliases = pSelecteredCharacter.Aliases.Where(s => !string.IsNullOrWhiteSpace(s)).Distinct().ToList();
+			if (aliases.Count != 0)
 			{
-				lblAliases.Text += alias + ", ";
+				StackLayout stack = stckAliases;
+
+				foreach (string alias in aliases)
+				{
+					Label lblAlias = new Label()
+					{
+						Text = alias,
+						TextColor = Color.Black ,
+						FontSize = 18
+					};
+					stack.Children.Add(lblAlias);
+				}
 			}
-			lblAliases.Text = lblAliases.Text.Substring(0, lblAliases.Text.Length - 2);
+			else
+			{
+				grdAliases.IsVisible = false;
+			}
+
+			//if (pSelecteredCharacter.Allegiances.Count != 0)
+			//{
+			//	foreach (string alias in pSelecteredCharacter.Allegiances)
+			//	{
+			//		//House house = await GetHouseNameById(alias, ListHouses);
+			//		//lblAllegiances.Text += await GetHouseNameById(alias, ListHouses) + ", ";
+
+			//	}
+			//	//lblAllegiances.Text = lblAllegiances.Text.Substring(0, lblAllegiances.Text.Length - 2);
+			//}
+			//else
+			//{
+			//	grdAlegiances.IsVisible = false;
+			//}
+
+			grdAlegiances.IsVisible = false;
+
+
+			if (pSelecteredCharacter.Gender != string.Empty)
+			{
+				lblGender.Text = pSelecteredCharacter.Gender;
+			}
+			else
+			{
+				grdGender.IsVisible = false;
+			}
+
+			if (pSelecteredCharacter.Culture != string.Empty)
+			{
+				lblCulture.Text = pSelecteredCharacter.Culture;
+			}
+			else
+			{
+				grdCulture.IsVisible = false;
+			}
+
+			if (pSelecteredCharacter.Born != string.Empty)
+			{
+				lblBorn.Text = pSelecteredCharacter.Born;
+			}
+			else
+			{
+				grdBorn.IsVisible = false;
+			}
 
 			if (pSelecteredCharacter.IsAlive)
 			{
-				lblDiedTip.IsVisible = false;
+				grdDiedTip.IsVisible = false;
 				lblIsAlive.Text = "Alive";
 			}
 			else
 			{
-				lblDiedTip.IsVisible = true;
 				lblDied.Text = pSelecteredCharacter.Died;
 				lblIsAlive.Text = "Died";
 			}
 
-			/*
-			 <Label x:Name="lblAliases" TextColor="Black" />
-			<Label x:Name="lblAllegiances" TextColor="Black" />
-			<Label x:Name="lblGender" TextColor="Black" />
-			<Label x:Name="lblCulture" TextColor="Black" />
-			<Label x:Name="lblBorn" TextColor="Black" />
-			<Label x:Name="lblIsAlive" TextColor="Black" />
-			<Label x:Name="lblDied" TextColor="Black" />
-			<Label x:Name="lblFather" TextColor="Black" />
-			<Label x:Name="lblMother" TextColor="Black" />
-			<Label x:Name="lblSpouse" TextColor="Black" />
-			 */
+			List<string> familyTree = new List<string>(new string[] { pSelecteredCharacter.Father, pSelecteredCharacter.Mother, pSelecteredCharacter.Spouse });
+			familyTree = familyTree.Where(s => !string.IsNullOrWhiteSpace(s)).Distinct().ToList();
 
+			if (familyTree.Count != 0)
+			{
+				if (pSelecteredCharacter.Father != string.Empty)
+				{
+					lblFather.Text = GetCharacterById(GetIdFromUrl(pSelecteredCharacter.Father), ListCharacters).Name;
+				}
+				else
+				{
+					lblFather.Text = "Unknown";
+				}
+
+				if (pSelecteredCharacter.Mother != string.Empty)
+				{
+					lblMother.Text = GetCharacterById(GetIdFromUrl(pSelecteredCharacter.Mother), ListCharacters).Name;
+				}
+				else
+				{
+					lblMother.Text = "Unknown";
+				}
+
+				if (pSelecteredCharacter.Spouse != string.Empty)
+				{
+					lblSpouse.Text = GetCharacterById(GetIdFromUrl(pSelecteredCharacter.Spouse), ListCharacters).Name;
+				}
+				else
+				{
+					lblSpouse.Text = "Unknown";
+				}
+			}
+			else
+			{
+				grdFamilyTree.IsVisible = false;
+			}
+		}
+
+		private int GetIdFromUrl(string pUrl)
+		{
+			return int.Parse(pUrl.Substring(pUrl.LastIndexOf('/') + 1, pUrl.Length - pUrl.LastIndexOf('/') - 1));
+		}
+
+		private Character GetCharacterById(int pId, List<Character> pList)
+		{
+			return ListCharacters.Find(character => character.Id == pId);
+		}
+		private async Task<string> GetHouseNameById(string pUrl, List<House> pList)
+		{
+			await GetHouses();
+			House house = ListHouses.Find(h => h.Url == pUrl);
+			return house.Name;
+		}
+
+		private async Task GetHouses()
+		{
+			if (ListHouses != null)
+			{
+				ListHouses = await WesterosManager.GetHousesAsync();
+			}
 		}
 	}
 }
