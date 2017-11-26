bdbccd90ec5632e6736d9248d81131e50cf8d79b
MoerkerkeLander <lander.moerkerke@howest.student.be>
2017-11-26 22:23:16 +0100

    Gathered Characters in HousePage

--- ASOIAF/ASOIAF/View/HousesPage.xaml.cs
+++ ASOIAF/ASOIAF/View/HousesPage.xaml.cs
@@ -14,18 +14,38 @@ namespace ASOIAF.View
 	public partial class HousesPage : ContentPage
 	{
 		private List<House> Houses { get; set; }
+		private List<Character> Characters;
 
 		public HousesPage()
 		{
 			InitializeComponent();
 			GetHouses();
+
 			btnBack.Clicked += BtnBack_Clicked;
 			lvwHouses.ItemSelected += LvwHouses_ItemSelected;
 		}
 
+		private  async void GetCharacters()
+		{
+			Characters = await WesterosManager.GetCharactersAsync();
+		}
+
 		private void LvwHouses_ItemSelected(object sender, SelectedItemChangedEventArgs e)
 		{
-			lvwHouses.SelectedItem = null;
+			if (lvwHouses.SelectedItem != null)
+			{
+				if (Characters != null)
+				{
+					House selectedHouse = lvwHouses.SelectedItem as House;
+					Navigation.PushAsync(new HousesDetail(selectedHouse, Houses, Characters));
+					lvwHouses.SelectedItem = null;
+				}
+				else
+				{
+					DisplayAlert("Error", "Please wait untill all the data is loaded.", "Ok");
+					lvwHouses.SelectedItem = null;
+				}
+			}
 		}
 
 		private void BtnBack_Clicked(object sender, EventArgs e)
@@ -35,6 +55,7 @@ namespace ASOIAF.View
 
 		private async void GetHouses()
 		{
+			GetCharacters();
 			Houses = await WesterosManager.GetHousesAsync();
 			lvwHouses.ItemsSource = Houses;
 		}
